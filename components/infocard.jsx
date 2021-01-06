400bef98515fa44b425751f434385a3492939bfc
Jeremy Leach <doveraudio@gmail.com>
2021-01-06 02:41:27 -0500

    added button for abebooks

--- components/infocard.jsx
+++ components/infocard.jsx
@@ -23,6 +23,12 @@ function InfoCard(props) {
     function openGoodreads() {
         window.open("https://www.goodreads.com/book/isbn/" + record.result.isbn[0], '_blank');
     }
+    function openGoodreads() {
+        window.open("https://www.goodreads.com/book/isbn/" + record.result.isbn[0], '_blank');
+    }
+    function openAbeBooks() {
+        window.open("https://www.abebooks.com/servlet/SearchResults?isbn=" + record.result.isbn[0], '_blank');
+    }
     const classes = useStyles();
     let record = {};
     if (props.record.book != undefined) {
@@ -34,7 +40,7 @@ function InfoCard(props) {
             <Card>
                 <CardContent>
                     <Typography variant="h5" component="h2" >{record.work.title}</Typography>
-                    <Typography variant="h5" component="h2">{record.result.subtitle}</Typography>
+                    <Typography variant="h5" component="h2">{record.work.subtitle}</Typography>
                     <Typography>{((record.result.author_name !== undefined ? (record.result.author_name.length > 1 ? record.result.author_name.join(', ') : record.result.author_name[0]) : ""))}</Typography>
                     <Typography className={classes.title} >{(record.result.isbn !== undefined ? `ISBN-10: ${record.result.isbn[0]}` : "No ISBN")}</Typography>
                     <Typography className={classes.title} >{(record.result.isbn !== undefined ? `ISBN-30: ${record.result.isbn[1]}` : "No ISBN 30")}</Typography>
@@ -43,6 +49,7 @@ function InfoCard(props) {
                 </CardContent>
                 <CardActions>
                     <Button onClick={openGoodreads}>Find on goodreads</Button>
+                    <Button onClick={openAbeBooks}>Find on Abe Books</Button>
                 </CardActions>
             </Card>
         </>
